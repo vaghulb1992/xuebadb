1e8744b9429cf8b12bc9109291893df1f1321158
Vaghul Aditya Balaji <vaghulb1992@gmail.com>
2018-12-12 11:18:26 -0800

    Updating PyPI github link

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as infile:
 
 setup(
     name='xuebadb',
-    version='1.2',
+    version='1.3',
     python_requires='>=3',
     packages= find_packages(exclude=['tests*']),
     install_requires=['mysql-connector-python', 'pandas', 'pyodbc', 'numpy', 'missingno', 'seaborn', 'matplotlib'],
@@ -13,7 +13,7 @@ setup(
     description='xuebadb is a python package that allows users to conveniently connect to and query from various database systems using a unified API. Currently, it supports connecting to MySQL and SQL Server database systems. The queries return Pandas dataframes which can then be cleaned up and analysed using one of the modules in the package.',
     long_description=readme,
     long_description_content_type='text/markdown',
-    url ='https://github.com/ubco-mds-2018-labs/data533_lab4_vaghul_jiachen',
+    url ='https://github.com/vaghulb1992/xuebadb',
     author='Vaghul Aditya Balaji',
     author_email ='vaghulb1992@gmail.com' 
 )
